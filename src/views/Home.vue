a87b6719e6218cb42d7f604f705ce5bc9d95360f
adamray <adamrayw@users.noreply.github.com>
2021-10-01 22:31:02 +0700

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio repellat rerum eligendi, harum, molestias nihil necessitatibus labore mollitia, aliquam nemo voluptatum magnam eveniet sed unde magni sint repudiandae itaque rem illum quo! Deserunt ipsum, pariatur ipsam accusantium corrupti porro libero repellat quasi fugit veritatis dolor nemo quo velit? Consectetur suscipit consequatur quia obcaecati dignissimos aliquid tenetur nesciunt eos dicta, provident minus, sunt aut impedit optio fugiat ex illo ipsa id inventore. Incidunt vero mollitia, quaerat hic delectus velit, consequuntur quibusdam deserunt atque culpa debitis quod libero ut sequi tenetur. Laborum ullam dolor quisquam animi consequatur, eveniet omnis ad iusto? Enim.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio repellat rerum eligendi, harum, molestias nihil necessitatibus labore mollitia, aliquam nemo voluptatum magnam eveniet sed unde magni sint repudiandae itaque rem illum quo! Deserunt ipsum, pariatur ipsam accusantium corrupti porro libero repellat quasi fugit veritatis dolor nemo quo velit? Consectetur suscipit consequatur quia obcaecati dignissimos aliquid tenetur nesciunt eos dicta, provident minus, sunt aut impedit optio fugiat ex illo ipsa id inventore. Incidunt vero mollitia, quaerat hic delectus velit, consequuntur quibusdam deserunt atque culpa debitis quod libero ut sequi tenetur. Laborum ullam dolor quisquam animi consequatur, eveniet omnis ad iusto? Enim.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
